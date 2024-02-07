a639845f427bd344477e951b4842251778cc0f5e
bitazaratustra <ebaldasseroni@gmail.com>
2024-02-07 15:59:11 +0100

    'homework'
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    On branch bitazaratustra
    Changes to be committed:
    modified:   .gitignore
    new file:   03-data-warehouse/extras/green_tripdata_2022-04.parquet
    new file:   03-data-warehouse/extras/green_tripdata_2022-07.parquet
    new file:   03-data-warehouse/extras/green_tripdata_2022-08.parquet
    new file:   03-data-warehouse/extras/green_tripdata_2022-09.parquet
    new file:   03-data-warehouse/extras/green_tripdata_2022-10.parquet
    new file:   03-data-warehouse/extras/green_tripdata_2022-11.parquet
    new file:   03-data-warehouse/extras/green_tripdata_2022-12.parquet
    modified:   03-data-warehouse/extras/web_to_gcs.py

--- 03-data-warehouse/extras/web_to_gcs.py
+++ 03-data-warehouse/extras/web_to_gcs.py
@@ -1,66 +1,34 @@
-import io
-import os
 import requests
-import pandas as pd
 from google.cloud import storage
 
-"""
-Pre-reqs: 
-1. `pip install pandas pyarrow google-cloud-storage`
-2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
-3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
-"""
-
-# services = ['fhv','green','yellow']
-init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
-# switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
-
-
-def upload_to_gcs(bucket, object_name, local_file):
+def upload_to_gcs(bucket_name, object_name, local_file):
     """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    Sube un archivo local a Google Cloud Storage.
     """
-    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-
     client = storage.Client()
-    bucket = client.bucket(bucket)
+    bucket = client.bucket(bucket_name)
     blob = bucket.blob(object_name)
     blob.upload_from_filename(local_file)
 
+def web_to_gcs(year, service, bucket_name):
+    """
+    Descarga archivos Parquet desde el web y los carga en Google Cloud Storage.
+    """
+    for i in range(1, 13):
+        month = str(i).zfill(2)
+        file_name = f"{service}_tripdata_{year}-{month}.parquet"
 
-def web_to_gcs(year, service):
-    for i in range(12):
-        
-        # sets the month part of the file_name string
-        month = '0'+str(i+1)
-        month = month[-2:]
+        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{file_name}"
 
-        # csv file_name
-        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
+        r = requests.get(url)
 
-        # download it using requests via a pandas df
-        request_url = f"{init_url}{service}/{file_name}"
-        r = requests.get(request_url)
-        open(file_name, 'wb').write(r.content)
-        print(f"Local: {file_name}")
+        if r.status_code == 200:
+            with open(file_name, 'wb') as f:
+                f.write(r.content)
+            upload_to_gcs(bucket_name, f"{service}/{file_name}", file_name)
+            print(f"Archivo {file_name} cargado correctamente en GCS.")
+        else:
+            print(f"Error al descargar {file_name}: {r.status_code}")
 
-        # read it back into a parquet file
-        df = pd.read_csv(file_name, compression='gzip')
-        file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
-        print(f"Parquet: {file_name}")
-
-        # upload it to gcs 
-        upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
-        print(f"GCS: {service}/{file_name}")
-
-
-web_to_gcs('2019', 'green')
-web_to_gcs('2020', 'green')
-# web_to_gcs('2019', 'yellow')
-# web_to_gcs('2020', 'yellow')
-
+bucket_name = "ny_green_taxi_bucket"  # Reemplaza con el nombre de tu bucket en Google Cloud Storage
+web_to_gcs('2022', 'green', bucket_name)
